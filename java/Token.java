52e7bdef39e48b4c22500ed5526c54e0e36f854d
antonioortegas <antonioortega@uma.es>
2024-03-05 20:50:25 +0100

    add first token types enum and token object

---
+++ java/Token.java
@@ -0,0 +1,25 @@
+package java;
+
+/**
+ * @Type One of the possible TokenTypes (IDENTIFIER, SEMICOLON, CLASS, ...)
+ * @Lexeme The actual word or symbol (var, while, !=, while...)
+ * @Literal Stored values ()
+ * @Line The line of the file where the Token is located
+ */
+class Token {
+    final TokenType type;
+    final String lexeme;
+    final Object literal;
+    final int line;
+
+    Token(TokenType type, String lexeme, Object literal, int line) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
+}
